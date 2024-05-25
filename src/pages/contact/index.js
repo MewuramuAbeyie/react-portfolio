14ec8ada309ca4f727ce450931403d97dde62902
Mewuramu Abeyie <mewuramu@gmail.com>
2024-05-25 14:06:18 +0000

    Update index.js

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import * as emailjs from "emailjs-com";
+import * as emailjs from "@emailjs/browser";
 import "./style.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { meta } from "../../content_option";
@@ -33,8 +33,11 @@ export const ContactUs = () => {
         contactConfig.YOUR_SERVICE_ID,
         contactConfig.YOUR_TEMPLATE_ID,
         templateParams,
-        contactConfig.YOUR_USER_ID
-      )
+        contactConfig.YOUR_USER_ID,
+        form.current, 
+        {
+        publicKey: contactConfig.YOUR_PUBLIC_KEY,
+      })
       .then(
         (result) => {
           console.log(result.text);
